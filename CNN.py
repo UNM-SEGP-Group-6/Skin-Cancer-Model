be45906dcb13090c44f1a469d202730925fd2b5b
Mahmoud Yasser Mokhtar Sallam <efyms24@nottingham.edu.my>
2026-01-15 00:08:07 +0800

    Add Base Template + Notes for Tech Specs [WIP]
    Early push to clone & progress remotely.

---
+++ CNN.py
@@ -0,0 +1,9 @@
+# Import Statements
+
+# Dataset Loading & Preprocessing [MRA-MIDAS]
+
+# Model Architecture [Mobile M]
+
+# Model Training & Testing
+
+# Main Function
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CNN
 CNN Model to be trained on the MRA-MIDAS dataset.
+
+MRA-MIDAS Dataset Source: https://doi.org/10.71718/15nz-jv40
